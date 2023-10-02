fb6dd20289dfbb0d8f7d2250ab60e6e1d9e8f939
shivamburnwal <shivam29a@gmail.com>
2023-10-02 20:58:28 +0530

    Context and data from database

--- Django Learnings/feed/views.py
+++ Django Learnings/feed/views.py
@@ -1,9 +1,17 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+
+from .models import Post
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.all()
+        return context
 
 def ErrorView(request):
     return render(request, 'error.html')
