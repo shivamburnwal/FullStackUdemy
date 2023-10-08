d16b600666c5d714ff7457e6fdcb7e4d1b8d3a44
shivamburnwal <shivam29a@gmail.com>
2023-10-08 14:30:09 +0530

    Added bootstrap css and updated pages.

--- Django Learnings/feed/forms.py
+++ Django Learnings/feed/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class PostForm(forms.Form):
-    text = forms.CharField(max_length=100, required=True)
     image = forms.FileField(required=True)
+    text = forms.CharField(max_length=100, required=True)
