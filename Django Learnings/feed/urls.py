931a6c9ddef1706d912bc076fdea4bd6b5778cba
shivamburnwal <shivam29a@gmail.com>
2023-10-08 12:06:31 +0530

    Added detail view page for posts.

--- Django Learnings/feed/urls.py
+++ Django Learnings/feed/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 # views
-from .views import HomeView, ErrorView
+from .views import HomeView, ErrorView, PostDetailView
 
 app_name = "feed"
 
 urlpatterns = [
     path('', HomeView.as_view(), name="index"),
+    path('detail/<int:pk>', PostDetailView.as_view(), name="detail"),
     path('error/', ErrorView, name="errorPage")
 ]
--- Django Learnings/feed/views.py
+++ Django Learnings/feed/views.py
@@ -1,5 +1,6 @@
 from typing import Any
 from django.shortcuts import render
+from django.views.generic import DetailView
 from django.views.generic.base import TemplateView
 
 from .models import Post
@@ -12,6 +13,10 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["posts"] = Post.objects.all()
         return context
+
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
 
 def ErrorView(request):
     return render(request, 'error.html')
